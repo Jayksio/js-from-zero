93aa4363ee7e450fb5cd817da3e841f7b2c345a6
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-08 15:47:23 +0100

    array

---
+++ 02. Control Flow/loop.js
@@ -0,0 +1,79 @@
+// if, else if, else
+
+// let a = 10;
+// let b = 20;
+// if (a >= b) {
+//     console.log(`a = ${a} est supérieur à b = ${b}`);
+// } else if (a <= b) {
+//     console.log(`a = ${a} est inférieur à b = ${b}`);
+// } else {
+//     console.log(`les problèmes`);
+// }
+
+// let password = 9;
+// if (password == 8) {
+//     console.log("Welcome");
+// } else if (password <= 8) {
+//     console.log("Password is too short");
+// } else if (password >= 8) {
+//     console.log("Password is too long");
+//     console.log("Password should be 8 caracters");
+// } else {
+//     console.log("Pls provide a password");
+// }
+
+// switch case
+
+// let fruit = "Orange";
+// fruit = fruit.toLowerCase();
+
+// switch (fruit) {
+//     case "banana":
+//         console.log("banana is good");
+//         break;
+//     case "orange":
+//         console.log("orange is good");
+//         break;
+//     case "apple":
+//         console.log("apple is good");
+//         break;
+//     default:
+//         console.log("I've never heard of that fruit");
+// }
+
+// for loop
+// DRY -> Don't Repeat Yourself
+// for (initial expression; condition; increment expression)
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// nested loop
+// for (let i = 0; i <= 5; i++) {
+//     console.log("Outer Loop", i)
+//     for (let j = 0; j <= 2; j++) {
+//         console.log("   Inner Loop", j);
+//     }
+// }
+
+
+// let firstName = "Pierre";
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${firstName} for ${i}`);
+// }
+
+// while
+// let i = 1;
+// while (i <= 10) {
+//     console.log("Hello world", i);
+//     i++;
+// }
+
+// do while
+// let i = 1;
+// do {
+//     console.log("Hello world", i);
+//     i++;
+// } while (i <= 5);
