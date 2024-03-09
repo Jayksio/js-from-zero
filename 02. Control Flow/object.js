0e0c748beb812f8c9c0f8c8f0975f5b7ff2cf449
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-09 22:54:59 +0100

    fin object, création functions

--- 02. Control Flow/object.js
+++ 02. Control Flow/object.js
@@ -1,12 +1,19 @@
 // déclaration d'un object basique
-let user = {
-    firstName: "Pierre",
-    lastName: "Leroy",
-    age: 30,
-    location: ['France', 'Paris'],
-    isProgrammer: true,
-}
+// let user = {
+//     firstName: "Pierre",
+//     lastName: "Leroy",
+//     age: 30,
+//     location: ['France', 'Paris'],
+// }
 
 // accéder à une valeur de l'object
 // console.log(user.firstName);
 // console.log(user['firstName']);
+
+// ajouter une valeur
+// user.isProgrammer = true;
+// console.log(user.isProgrammer);
+
+// supprimer une valeur
+// delete user.age;
+// console.log(user.age);
