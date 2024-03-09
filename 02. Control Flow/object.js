6d0193052e4748c4ce9b9a468ba77a8047e2ad68
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-09 22:22:26 +0100

    création du jeu WTO

---
+++ 02. Control Flow/object.js
@@ -0,0 +1,12 @@
+// déclaration d'un object basique
+let user = {
+    firstName: "Pierre",
+    lastName: "Leroy",
+    age: 30,
+    location: ['France', 'Paris'],
+    isProgrammer: true,
+}
+
+// accéder à une valeur de l'object
+// console.log(user.firstName);
+// console.log(user['firstName']);
