dd73455ce46f9a8cdd6b97c40b8d89877c27dfc6
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-10 21:17:33 +0100

    fin arrow functions et création object literals

---
+++ 02. Control Flow/arrow-functions.js
@@ -0,0 +1,39 @@
+// les arrow functions (fonctions fléchées) sont une syntaxe consive pour déclarer des fonctions.
+
+// function classique
+// function greet(username) {
+//     return `Hi ${username}`;
+// }
+// console.log(greet("Pierre"));
+
+// la même en arrow function
+// greet = (username) => {
+//     return `Hi ${username}`;
+// };
+// console.log(greet("Pierro"));
+
+// const double = (number) => {
+//     return number * 2;
+// };
+// console.log(double(5));
+
+// pour raccourcir encore plus, on peut retirer les parenthèses s'il n'y a qu'un paramètre, mais aussi le keyword return ainsi que les accolades
+// const double = n => n * 2;
+// console.log(double(5));
+
+
+setTimeout(() => {
+    console.log("Hello");
+    setTimeout(() => {
+        console.log("Coucou");
+        setTimeout(() => {
+            console.log("Namaste");
+            setTimeout(() => {
+                console.log("Hi");
+                setTimeout(() => {
+                    console.log("Bonjour");
+                }, 1000)
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000); 
