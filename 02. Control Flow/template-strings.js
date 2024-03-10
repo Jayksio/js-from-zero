38bfc9ee00fa7917708bb4661c6d4dd061e1998d
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-10 20:56:25 +0100

    fin template-strings et début arrow functions

---
+++ 02. Control Flow/template-strings.js
@@ -0,0 +1,11 @@
+// Un template string est une chaîne de caractères délimitée par des backticks (``) au lieu des traditionnelles simples ou doubles quotes. Cela permet d'inclure des variables ou des expressions à l'intérieur en utilisant la syntaxe ${expression}.
+// let firstName = "John";
+// let lastName = "Doe";
+// let age = 45;
+// console.log(`Hello ${firstName} ${lastName}, vous avez ${age} ans et vous allez donc avoir ${age + 1} ans l'année prochaine`);
+
+// On peut également appeler une fonction
+// function sayHello() {
+//     return "Coucou"
+// }
+// console.log(`${sayHello()}`);
