84f957dabf61a583db9284ef339508a2bc9d2497
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-11 18:29:26 +0100

    fin default-function-parameters

---
+++ 02. Control Flow/default-function-parameters.js
@@ -0,0 +1,9 @@
+/* les default function parameters sont des paramètres qui sont assignés par défaut lorsqu'il n'y a pas d'arguments définis, ça aide à gérer les éléments qui pourraient être manquants ou non définis */
+// function countTo5(count = true) {
+//     if (count === true) {
+//         for (let i = 1; i <= 5; i++) {
+//             console.log(`Count : ${i}`);
+//         }
+//     }
+// }
+// countTo5();
