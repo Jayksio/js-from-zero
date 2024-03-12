2de07a9da5e530acb5e77795c14e178883658d6e
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-12 21:53:51 +0100

    ajout map.js

---
+++ 02. Control Flow/for-each.js
@@ -0,0 +1,13 @@
+// let numbers = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+// function adder(number) {
+//     sum += number;
+// }
+// numbers.forEach(adder);
+// console.log(sum);
+
+// let colors = ["blue", "green", "purple", "red"];
+// let capColors = colors.forEach((color, i) => {
+//     colors[i] = color[0].toUpperCase() + color.substring(1);
+// });
+// console.log(colors);
