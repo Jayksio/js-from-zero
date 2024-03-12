d3fe76fbf1683d22d3cb982a1633837c5ced2273
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-12 10:38:48 +0100

    ajout for-in.js

---
+++ 02. Control Flow/ternaire.js
@@ -0,0 +1,12 @@
+// condition ? expressionIfTrue : expressionIfFalse;
+
+// let password = 9;
+// function passwordChecker(psw) {
+//     return psw >= 8 ? `strong password` : `weak password`;
+// }
+// let res = passwordChecker(password);
+// console.log(res);
+
+// let myMoney = 0;
+// let haveMoney = myMoney > 0 ? `buy products` : `bring money`;
+// console.log(haveMoney);
