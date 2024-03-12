919703a0480165f4ec51eca49ea959598471293b
Pierre Leroy <113354707+Jayksio@users.noreply.github.com>
2024-03-12 11:05:39 +0100

    ajout for-of.js

---
+++ 02. Control Flow/for-in.js
@@ -0,0 +1,28 @@
+// for (let key in object) {...}
+// une propriété est une paire clé-valeur, la clé est donc par exemple firstName, age...
+
+// objects
+// let user = {
+//     firstName: "Pierre",
+//     age: 30,
+//     work: "Programmer",
+// };
+// for (let key in user) {
+//     console.log(key, user[key]);
+// };
+
+// const object = {
+//     a: 1,
+//     b: 2,
+//     c: 3,
+// };
+// for (let k in object) {
+//     console.log(`key : ${k} - value : ${object[k]}`);
+// }
+
+// array
+// let list = [1, 2, 3];
+// for (let i in list) {
+//     console.log(i, list[i]);
+// }
+
